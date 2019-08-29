49d5e141f34258cddc8180ad4f75013072e9ef0f
Linuture <26655268+Linuture@users.noreply.github.com>
2019-08-29 16:49:37 +0800

    Add files via upload

---
+++ MyGeometry/My-Geometry.cpp
@@ -0,0 +1,9 @@
+#include "stdafx.h"
+
+#define MYGEOMETRY_EXPORT
+#include "My-Geometry.h"
+
+IMPLEMENT_SERIAL (MyGeometry, CObject, 1)
+//IMPLEMENT_SERIAL (MyPoint, MyGeometry, 1)
+//IMPLEMENT_SERIAL (MyLine, CObject, 1)
+//IMPLEMENT_SERIAL (MyPolygon, MyGeometry, 1)
